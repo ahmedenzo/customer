8640bf6aba52f38a2733aaf22bf826a92780d4a8
ahmed <ahmed.ferchichi@sesame.com.tn>
2023-03-01 16:30:51 +0100

    va

---
+++ src/test/java/org/sid/CustomerServer/CustomerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.CustomerServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
